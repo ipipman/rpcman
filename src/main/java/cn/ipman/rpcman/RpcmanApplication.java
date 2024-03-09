2bff4daa9bb4edfe47783553ffafba28a93e1d2f
ipipman <ipipman@163.com>
2024-03-09 17:47:20 +0800

    init project

---
+++ src/main/java/cn/ipman/rpcman/RpcmanApplication.java
@@ -0,0 +1,13 @@
+package cn.ipman.rpcman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcmanApplication.class, args);
+    }
+
+}
